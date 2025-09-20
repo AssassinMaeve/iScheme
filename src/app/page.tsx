d9740cec000a1891ffc633b2a7df2db33687df78
Nickson Anthony Pereira <120387886+NICK3ON@users.noreply.github.com>
2025-09-20 16:07:25 +0530

    removed duplicate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 "use client";
 import Link from "next/link";
 import NavBar from "./components/NavBar";
-
-import Footer from "./components/Footer"; // adjust path if needed
-import Link from "next/link";
 import ImageCard from "./components/ImageCard";
 import Footer from "./components/Footer";
 import { motion } from "framer-motion";
 import {SchemeViewer, Scheme} from "./components/Scheme";
 import rawSchemes from "@/data/all_schemes.json";
+import Questionnaire from "./questionnaire/Questionnaire";
+
 
 
 export default function Home() {
@@ -73,7 +72,7 @@ export default function Home() {
         <h1 className="text-7xl text-center mt-5">Your Gateway To <br />Every Scheme</h1>
         <div className="flex justify-center flex-col items-center">
           <p className="text-xl text-center mt-5 w-md">We help you find the Scheme best suited for you, weather you are a student, farmer, anyone.</p>
-          <Link href={"#"} className="px-6 py-3 bg-blue-600 text-white rounded-full font-semibold hover:bg-blue-700 hover:scale-105 transition duration-300 mt-10">Find Your Scheme now</Link>
+          <Link href={"/questionnaire"} className="px-6 py-3 bg-blue-600 text-white rounded-full font-semibold hover:bg-blue-700 hover:scale-105 transition duration-300 mt-10">Find Your Scheme now</Link>
         </div>
         <ImageCard></ImageCard>
 
